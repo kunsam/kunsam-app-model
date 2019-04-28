fa085cb85ed611e40e0706957a194c07f61d6450
kun <kunsam624@icloud.com>
2019-04-28 11:58:41 +0800

    add ITransactionContext

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,8 @@
 
 
 
-export * from './command'
 export * from './event'
-export * from './keyboard/keyboardmanager'
+export * from './command'
+export * from './transaction'
 export * from './mouse/mousemanager'
+export * from './keyboard/keyboardmanager'
